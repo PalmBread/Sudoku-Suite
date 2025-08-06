548262db9b6236a2e08a30382b6dd507b711d7bc
PalmBread <palmes.allanbrevin@gmail.com>
2025-08-06 11:28:19 +0800

    Added Union method

--- src/core/SudokuSolver.java
+++ src/core/SudokuSolver.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class SudokuSolver {
     public static void solve(SudokuBoard Board) {
-
+        easy_pass(Board);
     }
+
+    private static void easy_pass(SudokuBoard Board) {
+        
+    } 
 }
