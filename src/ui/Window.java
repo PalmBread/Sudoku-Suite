abeeb721cd4a7894c313f0c5efda19396bc4e01a
PalmBread <palmes.allanbrevin@gmail.com>
2025-08-06 11:09:58 +0800

    Restarting SudokuSolver + Removed Non Functional Keyboard Listener

--- src/ui/Window.java
+++ src/ui/Window.java
@@ -3,11 +3,7 @@ package ui;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import pages.SolverPage;
-
 import java.awt.CardLayout;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 public class Window {
     private JFrame frame = new JFrame("Sudoku Suite");
@@ -22,20 +18,6 @@ public class Window {
         this.frame.addKeyListener(null);
 
         this.panel.setFocusable(true);
-        this.panel.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                char c = e.getKeyChar();
-
-                if ("0123456789".indexOf(c) != -1) {
-                    String value = String.valueOf(c);
-
-                    value = value == "0" ? "" : value;
-
-                    SolverPage.activeNumber = value;
-                }
-            }
-        });
     }
 
     public void display(boolean value) {
